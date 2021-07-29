557491781af240553c1b8e5850d441bd62e642bf
Roman Zupancic <roman.k.zupancic@gmail.com>
2021-07-29 10:05:15 -0400

    Reworked Brainfuck state management

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,38 +2,53 @@ module Lib
     ( readInstructions
     ) where
 
+import Control.Monad (foldM)
+
+-- | The data `BrainfuckState` encapsultes the entire brainfuck state: memory and the current pointer index
+data BrainfuckState = BrainfuckState { prevMemory :: [Int]  -- All memory before the index pointer
+                                     , currMemory :: Int    -- The memory at the index pointer
+                                     , restMemory :: [Int]  -- All memory after the index pointer
+                                     } deriving (Show)
+
 readInstructions :: IO ()
 readInstructions = do
     putStrLn "Nope"
-    input <- getLine 
-    processInstructions input [0] 0
+    input <- getLine
+    runBrainfuckProgram "+++." 
     putStrLn "End"
+
+runBrainfuckProgram :: String -> IO BrainfuckState
+runBrainfuckProgram program = processInstructions program BrainfuckState { prevMemory = [], currMemory = 0, restMemory = [0] }
+
+processInstructions :: String -> BrainfuckState -> IO BrainfuckState
+processInstructions [] state = return state
+processInstructions xs state = foldM (flip processInstruction) state xs
 
 -- | Each call of processInstructions computes a single Brainfuck instruction,
 -- | and then recursively calls itself on remaining instructions
-processInstructions :: String -> [Int] -> Int -> IO [Int]
+processInstruction :: Char -> BrainfuckState -> IO BrainfuckState
 -- Compute memory manipulation
-processInstructions ('+':xs) memory index = processInstructions xs newMemory index
-    where
-        newMemory = replaceAtIndex memory (memory !! index + 1) index
-processInstructions ('-':xs) memory index = processInstructions xs newMemory index
-    where
-        newMemory = replaceAtIndex memory (memory !! index - 1) index
+processInstruction '+' state@BrainfuckState{currMemory = c} = return state { currMemory = c + 1 }
+processInstruction '-' state@BrainfuckState{currMemory = c} = return state { currMemory = c - 1 }
 
 -- Compute I/O
-processInstructions ('.':xs) memory index = do 
-    putStr $ show (memory !! index)
-    processInstructions xs memory index
-processInstructions (',':xs) memory index = return [2]
+processInstruction '.' state@BrainfuckState{currMemory = c} = do
+    putStr $ show c
+    return state
+-- TODO: Make this work
+processInstruction ',' state = return state
 
 -- Compute index movements
-processInstructions ('<':xs) memory index = processInstructions xs memory (index - 1)
-processInstructions ('>':xs) memory index = processInstructions xs memory (index + 1)
+processInstruction '<' state@(BrainfuckState [] _ _) = return state
+processInstruction '<' (BrainfuckState ps c r) = return BrainfuckState {prevMemory = init ps, currMemory = last ps, restMemory = c:r}
+processInstruction '>' (BrainfuckState p c []) = return BrainfuckState {prevMemory = p ++ [c], currMemory = 0, restMemory = []}
+processInstruction '>' (BrainfuckState p c (r:rs)) = return BrainfuckState {prevMemory = p ++ [c], currMemory = r, restMemory = rs}
 
 -- Compute loops
-processInstructions ('[':xs) memory index = return [2]
-processInstructions (']':xs) memory index = return [2]
-processInstructions _ memory _ = return memory 
+-- TODO: Make this work
+processInstruction '[' state = return state
+processInstruction ']' state = return state
+processInstruction _ state = return state
 
 replaceAtIndex :: [a] -> a -> Int -> [a]
 replaceAtIndex [] _ _ = []
